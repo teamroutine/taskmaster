43b1dc6cd1e1c14913a570519fcca317b0df095c
JouniAUT <jouni.au.tuomela@gmail.com>
2025-01-29 13:45:01 +0200

    Created method to get all teams

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/TeamRestController.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/TeamRestController.java
@@ -1,5 +1,41 @@
 package fi.haagahelia.taskmaster.taskmaster.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fi.haagahelia.taskmaster.taskmaster.domain.Team;
+import fi.haagahelia.taskmaster.taskmaster.domain.TeamRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/teams")
 public class TeamRestController {
+
+    @Autowired
+    private final TeamRepository teamRepository;
+
+    public TeamRestController(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+
+    }
+
+    // Get all the teams
+    @GetMapping
+    public ResponseEntity<List<Team>> getAllTeamns() {
+        List<Team> teams = teamRepository.findAll();
+        return ResponseEntity.ok(teams);
+    }
+    
+    
+    }
+    
 
 }

52e811e5ee310a4a7b7d64e6d655625956178854
Hilja Katajamäki <bhi033@myy.haaga-helia.fi>
2025-01-29 11:27:38 +0200

    Mockdata for database

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/TaskmasterApplication.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/TaskmasterApplication.java
@@ -1,13 +1,49 @@
 package fi.haagahelia.taskmaster.taskmaster;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.slf4j.Logger;
+import fi.haagahelia.taskmaster.taskmaster.domain.Panel;
+import fi.haagahelia.taskmaster.taskmaster.domain.PanelRepository;
+import fi.haagahelia.taskmaster.taskmaster.domain.Team;
+import fi.haagahelia.taskmaster.taskmaster.domain.TeamRepository;
 
 @SpringBootApplication
 public class TaskmasterApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(TaskmasterApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmasterApplication.class, args);
 	}
+
+	@Bean
+    public CommandLineRunner demo(PanelRepository prepository, TeamRepository trepository) {
+        return (args) -> {
+            log.info("Saving panels and team");
+			Team team1 = new Team(null, "TestTeam", "Team data for testing", null );
+			trepository.save(team1);
+			
+
+            Panel panel1 = new Panel(null, "Testi projekt", "testi projekti testaukseen");
+			
+            Panel panel2 = new Panel(null, "new project", "test for project testing");
+
+            prepository.save(panel1);
+			prepository.save(panel2);
+			
+			panel1.setTeam(team1);
+			panel2.setTeam(team1);
+	
+			prepository.save(panel1);
+			prepository.save(panel2);
+
+	
+           
+        };
+    }
 
 }

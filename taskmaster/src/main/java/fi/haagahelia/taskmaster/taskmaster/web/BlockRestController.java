480e9dd3eb7eff99b4cc63f2103d1c7a347127d8
Kosti Kangasmaa <kangasmaa95@gmail.com>
2025-01-29 12:15:24 +0200

    added Get all blocks endpoint

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/BlockRestController.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/BlockRestController.java
@@ -1,5 +1,31 @@
 package fi.haagahelia.taskmaster.taskmaster.web;
 
-public class BlockRestController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import fi.haagahelia.taskmaster.taskmaster.domain.BlockRepository;
+import fi.haagahelia.taskmaster.taskmaster.domain.Block;
+import fi.haagahelia.taskmaster.taskmaster.domain.PanelRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/blocks")
+public class BlockRestController {
+private final BlockRepository blockRepository;
+
+@Autowired
+public BlockRestController(BlockRepository blockRepository) {
+    this.blockRepository = blockRepository;
+}
+
+@GetMapping
+public ResponseEntity<List<Block>> getAllBlocks(){
+    List<Block> blocks = blockRepository.findAll();
+    return ResponseEntity.ok(blocks);
+}
 }

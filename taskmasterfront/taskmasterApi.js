130eef6b7c4632bdc3658043444c57a6f2fcd44b
IkeAni <ike.aniebonam@hotmail.com>
2025-01-30 16:03:14 +0200

    added fetch method to all panels

--- taskmasterfront/taskmasterApi.js
+++ taskmasterfront/taskmasterApi.js
@@ -7,3 +7,13 @@ export function fetchBlocks() {
             return response.json();
         });
 }
+
+export function fetchPanels() {
+    return fetch(import.meta.env.VITE_API_URL + "/panels")
+        .then(response => {
+            if (!response.ok) 
+                throw new Error("Error in fetch: " + response.statusText);
+            
+            return response.json();
+        });
+}

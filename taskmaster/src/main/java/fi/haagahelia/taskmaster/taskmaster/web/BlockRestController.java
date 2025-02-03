119459ba5f36321c90440d4c2ae3498cd99e5585
IkeAni <ike.aniebonam@hotmail.com>
2025-02-03 16:52:08 +0200

    added create block method

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/BlockRestController.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/BlockRestController.java
@@ -1,16 +1,23 @@
 package fi.haagahelia.taskmaster.taskmaster.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import fi.haagahelia.taskmaster.taskmaster.domain.BlockRepository;
 import fi.haagahelia.taskmaster.taskmaster.domain.Block;
 import fi.haagahelia.taskmaster.taskmaster.domain.PanelRepository;
+import io.micrometer.common.lang.NonNull;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @CrossOrigin
 @RestController
@@ -22,10 +29,17 @@ private final BlockRepository blockRepository;
 public BlockRestController(BlockRepository blockRepository) {
     this.blockRepository = blockRepository;
 }
-
+// Get all blocks
 @GetMapping
 public ResponseEntity<List<Block>> getAllBlocks(){
     List<Block> blocks = blockRepository.findAll();
     return ResponseEntity.ok(blocks);
 }
+// Create a new block
+@PostMapping
+@ResponseStatus(value = HttpStatus.CREATED, reason = "New block created")
+public Block createBlock(@RequestBody @NonNull Block newBlock) {
+    return blockRepository.save(newBlock);
+}
+
 }

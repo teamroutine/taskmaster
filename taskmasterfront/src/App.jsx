b9838e92f4010bc21a0c2a2e653a69bf0602c20d
Hilja Katajamäki <bhi033@myy.haaga-helia.fi>
2025-02-05 10:23:29 +0200

    Appbar/header

--- taskmasterfront/src/App.jsx
+++ taskmasterfront/src/App.jsx
@@ -1,9 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import taskmasterLogo from './assets/taskmaster-logo2.png'; 
 function App() {
     return (
-        <>
-        </>
+        <AppBar position="fixed" sx={{ width: '100%', backgroundColor: '#212121' }}>
+            <Toolbar >
+                <Typography variant="h6" style={{ flexGrow: 1 }}>
+                    <img 
+                     src={taskmasterLogo} 
+                        style={{ height: '75px' }} 
+                    />
+                </Typography>
+                    <Button color="inherit">Home</Button>
+                    <Button color="inherit">Products</Button>
+                    <Button color="inherit">Solutions</Button>
+                    <Button color="inherit">Contact</Button>
+
+                    <Button variant="outlined" color="inherit">Sign In</Button>
+                    <Button variant="outlined" color="inherit">Register</Button>
+            </Toolbar>
+        </AppBar>
     );
 }
 

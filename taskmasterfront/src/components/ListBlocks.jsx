cc309b2cd444996b102c025517bc96dc603b2357
Kosti Kangasmaa <kangasmaa95@gmail.com>
2025-02-05 10:17:03 +0200

    Converted listblocks to using mui paper and box

--- taskmasterfront/src/components/ListBlocks.jsx
+++ taskmasterfront/src/components/ListBlocks.jsx
@@ -1,31 +1,60 @@
-import { useEffect, useState } from 'react';
-import {useParams} from "react-router"
-import { fetchBlocks } from '../taskmasterApi';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchBlocksById } from "../taskmasterApi";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 
-function ListBlocks () {
-    const [blocks, setBlocks] = useState([]);
-    const [error, setError] = useState(null);
+const ListBlocks = () => {
+  const { panelid } = useParams();
+  const [blocks, setBlocks] = useState([]);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        fetchBlocks()
-            .then(data => setBlocks(data))
-            .catch(err => setError(err.message));
-    }, []);
+  useEffect(() => {
+    fetchBlocksById(panelid)
+      .then((data) => setBlocks(data))
+      .catch((err) => setError(err.message));
+  }, [panelid]);
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
+  if (error) {
+    return <Box>Error: {error}</Box>;
+  }
 
-    return (
-        <div>
-            <h1>Blocks List</h1>
-            <ul>
-                {blocks.map(block => (
-                    <li key={block.id}>{block.name}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <Box>
+      <h1>Blocks List</h1>
+      <Box sx={{ overflowX: "auto", whiteSpace: "nowrap" }}>
+        <Box
+          component="ul"
+          sx={{
+            display: "inline-block",
+            padding: 0,
+            margin: 0,
+            listStyleType: "none",
+          }}
+        >
+          {blocks.map((block) => (
+            <Box
+              component="li"
+              key={block.blockid}
+              sx={{ display: "inline-block", marginRight: 2 }}
+            >
+              <Paper
+                elevation={3}
+                sx={{
+                  width: 300,
+                  height: 800,
+                  padding: 2,
+                  textAlign: "center",
+                }}
+              >
+                {block.name}
+              </Paper>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default ListBlocks;

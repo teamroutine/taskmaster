92eedcaf51c9ef72b81398ece9a0b3e14f4b26a7
JouniAUT <jouni.au.tuomela@gmail.com>
2025-02-05 13:36:44 +0200

    TicketDTO class created

---
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/dto/TicketDTO.java
@@ -0,0 +1,67 @@
+package fi.haagahelia.taskmaster.taskmaster.dto;
+
+import fi.haagahelia.taskmaster.taskmaster.domain.Ticket;
+
+public class TicketDTO {
+    private Long ticketId;
+    private String ticketName;
+    private String description;
+    private Long blockId;
+
+    public TicketDTO() {
+
+    }
+
+    public TicketDTO(Long ticketId, String ticketName, String description, Long blockId) {
+        this.ticketId = ticketId;
+        this.ticketName = ticketName;
+        this.description = description;
+        this.blockId = blockId;
+    }
+
+    public TicketDTO(Ticket ticket) {
+        this.ticketId = ticket.getTicketId();
+        this.ticketName = ticket.getTicketName();
+        this.description = ticket.getDescription();
+        this.blockId = ticket.getBlock().getBlockId();
+    }
+
+    public Long getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Long ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getTicketName() {
+        return ticketName;
+    }
+
+    public void setTicketName(String ticketName) {
+        this.ticketName = ticketName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getBlockId() {
+        return blockId;
+    }
+
+    public void setBlockId(Long blockId) {
+        this.blockId = blockId;
+    }
+
+    @Override
+    public String toString() {
+        return "TicketDTO [ticketId=" + ticketId + ", ticketName=" + ticketName + ", description=" + description
+                + ", blockId=" + blockId + "]";
+    }
+
+}

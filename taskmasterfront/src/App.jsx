26d37b1cc264cffd441817e522be47fe53d29fcc
JouniAUT <jouni.au.tuomela@gmail.com>
2025-02-05 11:04:48 +0200

    Created link to Products page

--- taskmasterfront/src/App.jsx
+++ taskmasterfront/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
 import taskmasterLogo from './assets/taskmaster-logo2.png';
 import Home from "./pages/Home";
+import Products from "./pages/Products";
 
 function App() {
     return (
@@ -9,13 +10,11 @@ function App() {
             <AppBar position="fixed" sx={{ width: '100%', backgroundColor: '#212121' }}>
                 <Toolbar >
                     <Typography variant="h6" style={{ flexGrow: 1 }}>
-                        <img
-                            src={taskmasterLogo}
-                            style={{ height: '75px' }}
+                        <img src={taskmasterLogo} style={{ height: '75px' }}
                         />
                     </Typography>
                     <Button color="inherit" component={Link} to="/">Home</Button>
-                    <Button color="inherit">Products</Button>
+                    <Button color="inherit" component={Link} to="/products">Products</Button>
                     <Button color="inherit">Solutions</Button>
                     <Button color="inherit">Contact</Button>
 
@@ -26,7 +25,7 @@ function App() {
             <Container>
                 <Routes>
                     <Route path="/" element={<Home />} />
-
+                    <Route path="/products" element={<Products />} />
                 </Routes>
             </Container>
         </Router>

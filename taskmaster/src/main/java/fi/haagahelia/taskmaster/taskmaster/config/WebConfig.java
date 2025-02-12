06f7548826d2ad7b0ec877f769db79a0e91337d4
JouniAUT <jouni.au.tuomela@gmail.com>
2025-02-12 11:55:47 +0200

    First draft for CORS-configuration

---
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/config/WebConfig.java
@@ -0,0 +1,20 @@
+package fi.haagahelia.taskmaster.taskmaster.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @SuppressWarnings("null")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5174")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}

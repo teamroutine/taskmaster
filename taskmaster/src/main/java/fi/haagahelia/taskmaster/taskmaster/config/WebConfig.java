b38e78a487e3610422a884c8078c533c9fc72d11
JouniAUT <jouni.au.tuomela@gmail.com>
2025-02-12 11:59:28 +0200

    Added comments and allowed the right endpoint to access API

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/config/WebConfig.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/config/WebConfig.java
@@ -7,11 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // Allow only local frontend(endpoint: localhost:5173) request the API
     @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);

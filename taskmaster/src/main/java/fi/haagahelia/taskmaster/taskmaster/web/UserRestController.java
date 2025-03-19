feeea4b12ffcb11b687dcec0464fc42162bf69c4
Hilja Katajamäki <bhi033@myy.haaga-helia.fi>
2025-03-19 10:38:30 +0200

    ListUsers Rest controller- and fetch method

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/UserRestController.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/UserRestController.java
@@ -1,5 +1,33 @@
 package fi.haagahelia.taskmaster.taskmaster.web;
 
-public class UserRestController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fi.haagahelia.taskmaster.taskmaster.domain.AppUser;
+import fi.haagahelia.taskmaster.taskmaster.domain.AppUserRepository;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/users")
+public class UserRestController {
+    private final AppUserRepository appUserRepository;
+
+    @Autowired
+    public UserRestController(AppUserRepository appUserRepository){
+        this.appUserRepository = appUserRepository;
+    };
+    @GetMapping
+    public ResponseEntity<List<AppUser>>getAllAppUsers(){
+        List<AppUser> users = appUserRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+
+    
 }

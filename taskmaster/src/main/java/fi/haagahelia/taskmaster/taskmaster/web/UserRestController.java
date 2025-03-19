8606710313addf8aa8130eeb5e4020283d668b65
Hilja Katajamäki <bhi033@myy.haaga-helia.fi>
2025-03-19 10:51:28 +0200

    GetAppuserById RestController method

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/UserRestController.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/UserRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import fi.haagahelia.taskmaster.taskmaster.domain.AppUser;
 import fi.haagahelia.taskmaster.taskmaster.domain.AppUserRepository;
 
 @CrossOrigin
+
 @RestController
 @RequestMapping("/api/users")
 public class UserRestController {
@@ -22,12 +24,19 @@ public class UserRestController {
     public UserRestController(AppUserRepository appUserRepository){
         this.appUserRepository = appUserRepository;
     };
+    //Get All users
     @GetMapping
     public ResponseEntity<List<AppUser>>getAllAppUsers(){
         List<AppUser> users = appUserRepository.findAll();
         return ResponseEntity.ok(users);
     }
-
+    //Get one user
+    @GetMapping("/{id}")
+    public ResponseEntity<AppUser> getAppuserById(@PathVariable Long id){
+        return appUserRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
     
 }

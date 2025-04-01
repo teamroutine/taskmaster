6b163747ad83741d65deb0ddef6bee4e36edb251
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-01 10:37:54 +0300

    Created class for generating and handling JSONwebTokens

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/dto/AccessTokenPayloadDto.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/dto/AccessTokenPayloadDto.java
@@ -1,5 +1,36 @@
 package fi.haagahelia.taskmaster.taskmaster.dto;
 
-public class AccessTokenPayloadDto {
+import java.time.Instant;
 
+public class AccessTokenPayloadDto {
+    private String accessToken;
+    private Instant expiresAt;
+
+    public AccessTokenPayloadDto(String accessToken, Instant expiresAt) {
+        this.accessToken = accessToken;
+        this.expiresAt = expiresAt;
+    }
+
+    public AccessTokenPayloadDto() {
+
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+
+    }
+
+    public Instant getExpiresAt() {
+        return expiresAt;
+
+    }
+
+    public void setExpiresAt(Instant expiresAt) {
+        this.expiresAt = expiresAt;
+    }
 }
--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/service/JwtService.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/service/JwtService.java
@@ -1,5 +1,69 @@
 package fi.haagahelia.taskmaster.taskmaster.service;
 
+import org.springframework.http.HttpHeaders;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.sql.Date;
+import java.time.Duration;
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import fi.haagahelia.taskmaster.taskmaster.dto.AccessTokenPayloadDto;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Service
 public class JwtService {
+    private final long EXPIRATION_TIME = Duration.ofHours(8).toMillis();
+    private final String PREFIX = "Bearer ";
+
+    @Value("${auth.jwt-secret}")
+    private String jwtSecret;
+
+    public AccessTokenPayloadDto getAccessToken(String username) {
+        Instant expiresAt = Instant.now().plusMillis(EXPIRATION_TIME);
+        String accessToken = Jwts.builder()
+                .subject(username)
+                .expiration(Date.from(expiresAt))
+                .signWith(getSigningKey())
+                .compact();
+
+        return new AccessTokenPayloadDto(accessToken, expiresAt);
+    }
+
+    public String getAuthUser(HttpServletRequest request) {
+        String authorizationHeaderValue = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authorizationHeaderValue == null) {
+            return null;
+        }
+
+        JwtParser parser = getJwtParser();
+
+        try {
+            String user = parser
+                    .parseClaimsJws(authorizationHeaderValue.replace(PREFIX, ""))
+                    .getBody().getSubject();
+
+            return user;
+
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = jwtSecret.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private JwtParser getJwtParser() {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build();
+    }
 
 }

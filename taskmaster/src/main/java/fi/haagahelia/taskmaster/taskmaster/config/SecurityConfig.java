6777e640ba452295d8899787a04cb77def47f0c6
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-02 23:07:01 +0300

    Removed useless import

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/config/SecurityConfig.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package fi.haagahelia.taskmaster.taskmaster.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;

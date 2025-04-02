98860214c130b4f4fc02399c263958ef16b7511e
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-02 12:25:17 +0300

    Updated findByUsername method into repository

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/domain/AppUserRepository.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/domain/AppUserRepository.java
@@ -1,10 +1,14 @@
 package fi.haagahelia.taskmaster.taskmaster.domain;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AppUserRepository extends JpaRepository <AppUser,Long>{
-      List<AppUser> findByFirstName(String fisrtName);
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+      List<AppUser> findByFirstName(String firstName);
+
+      Optional<AppUser> findByUsername(String username);
+
+      Optional<AppUser> findOneById(Long id);
 
 }

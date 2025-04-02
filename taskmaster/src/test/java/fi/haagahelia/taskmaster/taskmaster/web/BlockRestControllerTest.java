8bc6267dc3d31ea52467239cee9355ccd6e5c432
IkeAni <ike.aniebonam@hotmail.com>
2025-04-02 12:11:51 +0300

    getallblocks test minor fix

--- taskmaster/src/test/java/fi/haagahelia/taskmaster/taskmaster/web/BlockRestControllerTest.java
+++ taskmaster/src/test/java/fi/haagahelia/taskmaster/taskmaster/web/BlockRestControllerTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +20,7 @@ import fi.haagahelia.taskmaster.taskmaster.domain.Block;
 import fi.haagahelia.taskmaster.taskmaster.domain.BlockRepository;
 import fi.haagahelia.taskmaster.taskmaster.web.BlockRestController;
 
+@SpringBootTest 
 @WebMvcTest(BlockRestController.class)
 class BlockRestControllerTest {
 

40782300aed78f75af9742f88527487b97ba82e4
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-02 10:06:55 +0300

    LoginUserDto created

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/AuthRestController.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/web/AuthRestController.java
@@ -1,5 +1,36 @@
 package fi.haagahelia.taskmaster.taskmaster.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fi.haagahelia.taskmaster.taskmaster.dto.LoginUserDto;
+import fi.haagahelia.taskmaster.taskmaster.service.JwtService;
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+
 public class AuthRestController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+@PostMapping("/login")
+public ResponseEntity<?> login(@Valid @RequestBody LoginUserDto login, BindingResult bindingResult)
+
+
+
 
 }

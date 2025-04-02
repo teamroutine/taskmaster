45b6a2a336a953e8444f965a6b67b8172e66ab8f
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-02 10:44:00 +0300

    Add constructors, getters and setters

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/dto/RegisterUserDto.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/dto/RegisterUserDto.java
@@ -1,5 +1,36 @@
 package fi.haagahelia.taskmaster.taskmaster.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class RegisterUserDto {
+    @NotBlank(message = "Username is required")
+    String username;
+    @NotBlank(message = "Password is required")
+    String password;
+
+    public RegisterUserDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public RegisterUserDto() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }

1592fb6a216e8a43cdf1776a3c5b57954fbaaa70
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-02 13:10:38 +0300

    Fix minor problems

---
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/service/AppUserService.java
@@ -0,0 +1,56 @@
+package fi.haagahelia.taskmaster.taskmaster.service;
+
+import java.util.Optional;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import fi.haagahelia.taskmaster.taskmaster.domain.AppUser;
+import fi.haagahelia.taskmaster.taskmaster.domain.AppUserRepository;
+import fi.haagahelia.taskmaster.taskmaster.dto.RegisterUserDto;
+
+@Service
+public class AppUserService {
+
+    private AppUserRepository appUserRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public AppUserService(AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
+        this.appUserRepository = appUserRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public AppUser registerUser(RegisterUserDto registerUserDto) {
+        String encodedPassword = passwordEncoder.encode(registerUserDto.getPassword());
+
+        AppUser newUser = new AppUser();
+        newUser.setFirstName(registerUserDto.getFirstName());
+        newUser.setLastName(registerUserDto.getLastName());
+        newUser.setUsername(registerUserDto.getUsername());
+        newUser.setEmail(registerUserDto.getEmail());
+        newUser.setPhone(registerUserDto.getPhone());
+        newUser.setPassword(encodedPassword);
+
+        return appUserRepository.save(newUser);
+    }
+
+    public Optional<AppUser> getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication instanceof UsernamePasswordAuthenticationToken token && token.getPrincipal() != null) {
+            return Optional.empty();
+
+        }
+
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        if (token.getPrincipal() == null) {
+            return Optional.empty();
+        }
+
+        return appUserRepository.findByUsername(token.getPrincipal().toString());
+    }
+
+}

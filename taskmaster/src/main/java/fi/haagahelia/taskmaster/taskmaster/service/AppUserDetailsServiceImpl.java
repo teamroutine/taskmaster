9a2442bb0b6fa06f714438911db6855e9cff6f1e
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-02 22:55:14 +0300

    Added Service notation into the class

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/service/AppUserDetailsServiceImpl.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/service/AppUserDetailsServiceImpl.java
@@ -3,9 +3,11 @@ package fi.haagahelia.taskmaster.taskmaster.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import fi.haagahelia.taskmaster.taskmaster.domain.AppUserRepository;
 
+@Service
 public class AppUserDetailsServiceImpl implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;

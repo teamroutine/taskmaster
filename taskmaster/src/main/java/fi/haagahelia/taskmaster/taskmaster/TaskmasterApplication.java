9fbb7fcdc97f9250dee0d2fc4af26f833946edb3
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-03 14:16:21 +0300

    Added encryption for testuser's password

--- taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/TaskmasterApplication.java
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/TaskmasterApplication.java
@@ -1,11 +1,12 @@
 package fi.haagahelia.taskmaster.taskmaster;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.slf4j.Logger;
 import fi.haagahelia.taskmaster.taskmaster.domain.Panel;
 import fi.haagahelia.taskmaster.taskmaster.domain.PanelRepository;
@@ -21,6 +22,9 @@ import fi.haagahelia.taskmaster.taskmaster.domain.AppUserRepository;
 @SpringBootApplication
 public class TaskmasterApplication {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	private static final Logger log = LoggerFactory.getLogger(TaskmasterApplication.class);
 
 	public static void main(String[] args) {
@@ -35,9 +39,9 @@ public class TaskmasterApplication {
 			Team team1 = new Team(null, "Team1", "description for team 1", null, null);
 			terepository.save(team1);
 
-			AppUser appuser1 = new AppUser(null, "Hilja", "Katajamäki", "hilja.example@gmail", null, "hilja123",
-					"salasana");
-
+			String encodedPassword = passwordEncoder.encode("salasana");
+			AppUser appuser1 = new AppUser(null, "Hilja", "Katajamäki", "hilja.example@gmail", "0123456789", "hilja123",
+					encodedPassword);
 			arepository.save(appuser1);
 
 			Panel panel1 = new Panel(null, "Project 1", "description for panel 1 ", team1, null);

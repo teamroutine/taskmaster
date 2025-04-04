90f24e006129f93d449770301b7af6420d284f70
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-04 13:34:14 +0300

    Created a route for registration

--- taskmasterfront/src/App.jsx
+++ taskmasterfront/src/App.jsx
@@ -8,6 +8,7 @@ import CreatePanel from "./components/CreatePanel";
 import ListPanelView from "./components/ListPanelView";
 import ListTeams from "./components/ListTeams";
 import theme from "./theme";
+import Register from "./components/Register";
 
 function App() {
     return (
@@ -21,11 +22,11 @@ function App() {
                         <Typography variant="h6" style={{ flexGrow: 1 }}>
                             <img src={taskmasterLogo} style={{ height: "75px" }} alt="Taskmaster Logo" />
                         </Typography>
-                        <Button color="inherit" component={Link} to="/" sx={{ marginRight: 2,fontSize: '1.05rem'  }}>Home</Button>
+                        <Button color="inherit" component={Link} to="/" sx={{ marginRight: 2, fontSize: '1.05rem' }}>Home</Button>
                         <Button color="inherit" component={Link} to="/panels" sx={{ marginRight: 2, fontSize: '1.05rem' }}>All Panels</Button>
                         <Button color="inherit" component={Link} to="/teams" sx={{ marginRight: 2, fontSize: '1.05rem' }}>All Teams</Button>
                         <Button variant="outlined" color="inherit" sx={{ marginRight: 1 }}>Sign In</Button>
-                        <Button variant="outlined" color="inherit">Register</Button>
+                        <Button variant="outlined" color="inherit" component={Link} to="/register" sx={{ marginRight: 2, fontSize: '1.05rem' }}>Register</Button>
                     </Toolbar>
                 </AppBar>
                 <Container>
@@ -35,7 +36,8 @@ function App() {
                         <Route path="/panels" element={<ListPanelView />} />
                         <Route path="/panels/:panelid" element={<PanelView />} />
                         <Route path="/panel/create" element={<CreatePanel />} />
-                        <Route path="/teams" element={<ListTeams/>}/>
+                        <Route path="/teams" element={<ListTeams />} />
+                        <Route path="/register" element={<Register />} />
                     </Routes>
                 </Container>
             </Router>

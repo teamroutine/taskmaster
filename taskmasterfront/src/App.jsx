3182293412ca8c49b6fb2a2fb942fcecb3eebf99
Hilja Katajamäki <bhi033@myy.haaga-helia.fi>
2025-04-09 11:59:01 +0300

    Deleted all panels page

--- taskmasterfront/src/App.jsx
+++ taskmasterfront/src/App.jsx
@@ -5,7 +5,6 @@ import taskmasterLogo from "./assets/taskmaster-logo2.png";
 import Home from "./pages/Home";
 import PanelView from "./components/PanelView";
 import CreatePanel from "./components/CreatePanel";
-import ListPanelView from "./components/ListPanelView";
 import ListTeams from "./components/ListTeams";
 import theme from "./theme";
 import Register from "./components/Register";
@@ -26,7 +25,6 @@ function App() {
                             <img src={taskmasterLogo} style={{ height: "75px" }} alt="Taskmaster Logo" />
                         </Typography>
                         <Button color="inherit" component={Link} to="/" sx={{ marginRight: 2, fontSize: '1.05rem' }}>Home</Button>
-                        <Button color="inherit" component={Link} to="/panels" sx={{ marginRight: 2, fontSize: '1.05rem' }}>All Panels</Button>
                         <Button color="inherit" component={Link} to="/teams" sx={{ marginRight: 2, fontSize: '1.05rem' }}>All Teams</Button>
                         {/* <Button variant="outlined" color="inherit" component={Link} to="/login" sx={{ marginRight: 1 }}>Login</Button> */}
                         <AuthButton variant='outlined' color='inherit' component={Link} to="/login" sx={{ marginRight: 1 }} />
@@ -38,7 +36,6 @@ function App() {
                     <Toolbar />
                     <Routes>
                         <Route path="/" element={<Home />} />
-                        <Route path="/panels" element={<ListPanelView />} />
                         <Route path="/panels/:panelid" element={<PanelView />} />
                         <Route path="/panel/create" element={<CreatePanel />} />
                         <Route path="/teams" element={<ListTeams />} />

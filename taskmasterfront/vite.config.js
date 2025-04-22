ea512bd61dda6bd7d26502e467e66e53b9139b84
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-22 14:49:27 +0300

    Created frontend test coverage for the project.

--- taskmasterfront/vite.config.js
+++ taskmasterfront/vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
+    include: [
+      'src/tests/**/*.{test,spec}.{js,ts,jsx,tsx}',
+      'src/tests/*.{test,spec}.{js,ts,jsx,tsx}'
+    ],
     coverage: {
-      provider: 'istanbul',
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['src/**/*.{js,ts,jsx,tsx}'],
+      dir: 'coverage',
     },
   },
 });

ae89ef85ebe8f203a8abb3e6d1e8b0b8f91e24f1
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-23 14:40:49 +0300

    Created a function for adding a new user to team.

---
+++ taskmaster/src/main/java/fi/haagahelia/taskmaster/taskmaster/service/TeamService.java
@@ -0,0 +1,49 @@
+package fi.haagahelia.taskmaster.taskmaster.service;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fi.haagahelia.taskmaster.taskmaster.domain.AppUser;
+import fi.haagahelia.taskmaster.taskmaster.domain.AppUserRepository;
+import fi.haagahelia.taskmaster.taskmaster.domain.InviteRepository;
+import fi.haagahelia.taskmaster.taskmaster.domain.Team;
+import fi.haagahelia.taskmaster.taskmaster.domain.TeamRepository;
+
+import fi.haagahelia.taskmaster.taskmaster.domain.Invite;
+
+@Service
+public class TeamService {
+
+    private final InviteRepository inviteRepository;
+    private final TeamRepository teamRepository;
+    private final AppUserRepository appUserRepository;
+
+    public TeamService(InviteRepository inviteRepository, TeamRepository teamRepository,
+            AppUserRepository appUserRepository) {
+        this.inviteRepository = inviteRepository;
+        this.teamRepository = teamRepository;
+        this.appUserRepository = appUserRepository;
+    }
+
+    public void addUserToTeamWithInvite(String inviteCode, String username) {
+        Invite invite = inviteRepository.findByNanoId(inviteCode)
+                .orElseThrow(() -> new RuntimeException("Invite link not found."));
+
+        if (invite.getExpiresAt().before(new Date())) {
+            throw new RuntimeException("Invite link has expired");
+        }
+        Team team = invite.getTeam();
+        AppUser appUser = appUserRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found."));
+
+        if (team.getAppUsers().contains(appUser)) {
+            throw new RuntimeException("User is already a member of the team.");
+
+        }
+
+        team.getAppUsers().add(appUser);
+        teamRepository.save(team);
+    }
+}

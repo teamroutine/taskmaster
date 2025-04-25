0768cc45c36220dc7765d346f89239614d514210
JouniAUT <jouni.au.tuomela@gmail.com>
2025-04-25 12:12:43 +0300

    Added info about application and button for registration on Home Page.

--- taskmasterfront/src/pages/Home.jsx
+++ taskmasterfront/src/pages/Home.jsx
@@ -1,6 +1,20 @@
+import { Box, Typography, Button } from "@mui/material";
+import Register from "../components/Register";
+
+
 function Home() {
     return (
-        <h1>Home page</h1>
+        <Box sx={{ textAlign: "center", mt: 5 }} >
+            <Typography variant="h1" gutterBottom>
+                Taskmaster
+            </Typography>
+            <Typography variant="h6">
+                Welcome to Taskmaster! This is a platform for planning, organizing, managing and tracking your all kind of projects.
+                Get started by logging in and creating your own team. Taskmaster lets you collaborate with your friends and colleagues
+                effectively.
+            </Typography>
+            <Register>Register</Register>
+        </Box>
     )
 }
 
